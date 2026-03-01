aacaf12c7e1716bea13e0debf9688b917dced9c0
codeforge11 <codeforge11@gmail.com>
2026-03-01 13:07:32 +0100

    feat: include active bullets count in debug view

--- src/debuger.go
+++ src/debuger.go
@@ -13,11 +13,13 @@ func (g *Game) DebugView(screen *ebiten.Image) {
 			"Seconds from startup : %d\n"+
 			"FPS: %.2f\n"+
 			"Player cords: X: %.2f, Y: %.2f \n"+
+			"Active bullets: %d\n"+
 			"Creator: codeforge11",
 		g.Count/60,
 		ebiten.ActualFPS(),
 		g.PlayerX,
 		g.PlayerY,
+		len(g.Bullets),
 	)
 
 	ebitenutil.DebugPrint(screen, msg)
