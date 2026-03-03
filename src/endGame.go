07c844ff420dbee0605cf5a137efdd19773a01d3
codeforge11 <codeforge11@gmail.com>
2026-03-03 12:04:36 +0100

    fix: adjust text positioning for end game win and lose screens

--- src/endGame.go
+++ src/endGame.go
@@ -13,7 +13,7 @@ func (g *Game) endGameWin(screen *ebiten.Image) {
 	miniText2 := "Click ESC to return to menu"
 
 	titleDrawOptions := &text.DrawOptions{}
-	titleDrawOptions.GeoM.Translate(100, 50)
+	titleDrawOptions.GeoM.Translate(50, 50)
 	titleDrawOptions.ColorScale.ScaleWithColor(color.White)
 
 	text.Draw(screen, title, &text.GoTextFace{
@@ -22,7 +22,7 @@ func (g *Game) endGameWin(screen *ebiten.Image) {
 	}, titleDrawOptions)
 
 	miniText1DrawOptions := &text.DrawOptions{}
-	miniText1DrawOptions.GeoM.Translate(100, 150)
+	miniText1DrawOptions.GeoM.Translate(50, 150)
 	miniText1DrawOptions.ColorScale.ScaleWithColor(color.White)
 
 	text.Draw(screen, miniText1, &text.GoTextFace{
@@ -31,7 +31,7 @@ func (g *Game) endGameWin(screen *ebiten.Image) {
 	}, miniText1DrawOptions)
 
 	miniText2DrawOptions := &text.DrawOptions{}
-	miniText2DrawOptions.GeoM.Translate(100, 200)
+	miniText2DrawOptions.GeoM.Translate(50, 200)
 	miniText2DrawOptions.ColorScale.ScaleWithColor(color.White)
 
 	text.Draw(screen, miniText2, &text.GoTextFace{
@@ -42,5 +42,34 @@ func (g *Game) endGameWin(screen *ebiten.Image) {
 }
 
 func (g *Game) endGameLose(screen *ebiten.Image) {
+	title := "YOU LOSE :("
+	miniText1 := "Click space to restart..."
+	miniText2 := "Click ESC to return to menu"
 
+	titleDrawOptions := &text.DrawOptions{}
+	titleDrawOptions.GeoM.Translate(50, 50)
+	titleDrawOptions.ColorScale.ScaleWithColor(color.White)
+
+	text.Draw(screen, title, &text.GoTextFace{
+		Source: fontFace,
+		Size:   18, //Text font size
+	}, titleDrawOptions)
+
+	miniText1DrawOptions := &text.DrawOptions{}
+	miniText1DrawOptions.GeoM.Translate(50, 150)
+	miniText1DrawOptions.ColorScale.ScaleWithColor(color.White)
+
+	text.Draw(screen, miniText1, &text.GoTextFace{
+		Source: fontFace,
+		Size:   12, //Text font size
+	}, miniText1DrawOptions)
+
+	miniText2DrawOptions := &text.DrawOptions{}
+	miniText2DrawOptions.GeoM.Translate(50, 200)
+	miniText2DrawOptions.ColorScale.ScaleWithColor(color.White)
+
+	text.Draw(screen, miniText2, &text.GoTextFace{
+		Source: fontFace,
+		Size:   12, //Text font size
+	}, miniText2DrawOptions)
 }
