f339c5304895869e72a8b1e2ef0bac0b84ccd8df
codeforge11 <codeforge11@gmail.com>
2026-09-23 15:03:04 +0200

    feat: add showing real TPS number in debug mode

--- src/debuger.go
+++ src/debuger.go
@@ -12,6 +12,7 @@ func (g *Game) DebugView(screen *ebiten.Image) {
 		"Welcome in debug mode\n"+
 			"Seconds from startup : %d\n"+
 			"FPS: %.2f\n"+
+			"TPS: %.2f\n"+
 			"Player cords: X: %.2f, Y: %.2f \n"+
 			"Active bullets: %d\n"+
 			"Active enemies: %d\n"+
@@ -19,6 +20,7 @@ func (g *Game) DebugView(screen *ebiten.Image) {
 			"Creator: codeforge11",
 		g.Count/60,
 		ebiten.ActualFPS(),
+		ebiten.ActualTPS(),
 		g.PlayerX,
 		g.PlayerY,
 		len(g.Bullets),
